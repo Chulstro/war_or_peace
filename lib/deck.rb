bb330121e19d733547d496a039a0bfd6f155d7e4
Chandler Hulstrom <chulstro11@gmail.com>
2020-05-07 09:49:39 -0600

    Updated remove card method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,37 @@
 require './lib/card'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards,
+              :high_cards
 
   def initialize(cards)
     @cards = cards
+    @high_cards = []
   end
 
   def rank_of_card_at(num)
     ranking_card = @cards[num]
     ranking_card.rank
   end
+
+  def high_ranking_cards
+    # require 'pry'; binding.pry
+    @cards.each do |card|
+      if card.rank >= 11
+        @high_cards << card
+      end
+    end
+    @high_cards
+  end
+
+  def percent_high_ranking
+    percent = @high_cards.count.to_f / @cards.count.to_f * 100
+    percent.round(2)
+  end
+
+  def remove_card
+    @cards.delete_at(0)
+  end
+
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,7 +9,9 @@ class DeckTest < Minitest::Test
     card2 = Card.new(:spade, "3", 3)
     card3 = Card.new(:heart, "Ace", 14)
     cards = []
-    cards << [card1, card2, card3]
+    cards << card1
+    cards << card2
+    cards << card3
 
     deck = Deck.new(cards)
 
@@ -29,4 +31,66 @@ class DeckTest < Minitest::Test
 
     assert_equal deck.rank_of_card_at(0), 12
   end
+
+  def test_high_ranking_array
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = []
+    cards << card1
+    cards << card2
+    cards << card3
+
+    deck = Deck.new(cards)
+
+    assert_equal deck.high_ranking_cards.length, 2
+  end
+
+  def test_it_calculates_percent
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = []
+    cards << card1
+    cards << card2
+    cards << card3
+
+    deck = Deck.new(cards)
+    deck.high_ranking_cards
+
+    assert_equal deck.percent_high_ranking, 66.67
+  end
+
+  def test_remove_card_and_update_deck
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = []
+    cards << card1
+    cards << card2
+    cards << card3
+
+    deck = Deck.new(cards)
+
+    assert_equal deck.remove_card, card1
+    assert_equal deck.cards.count, 2
+  end
+
+  def test_if_card_added
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = []
+    cards << card1
+    cards << card2
+    cards << card3
+
+    deck = Deck.new(cards)
+
+    card4 = Card.new(:Club, "4", 4)
+    deck.add_card(card4)
+
+    
+  end
+
 end
