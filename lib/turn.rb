56771a3527f915b4625020b3b2aba9aea83fab37
Chandler Hulstrom <chulstro11@gmail.com>
2020-05-09 12:21:29 -0600

    Initial win condition tested

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class Turn
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+
+  def initialize(name, name2)
+    @player1 = name
+    @player2 = name2
+    @spoils_of_war = []
+  end
+
+  def type
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    else
+      :war
+    end
+  end
+
+  def winner
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1
+      else
+        player2
+      end
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      else
+        player2
+      end
+    elsif type == :mutually_assured_destruction
+      "All Casualties"
+    end
+  end
+
+end
